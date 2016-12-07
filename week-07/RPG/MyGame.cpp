be16ebab9261e9a4262426f9837828394fe47913
Kata Almasy <almasy.kata@gmail.com>
2016-12-07 11:02:38 +0100

    pressing arrow down makes the hero move down

--- week-07/RPG/MyGame.cpp
+++ week-07/RPG/MyGame.cpp
@@ -1,8 +1,7 @@
 #include <vector>
+#include <iostream>
 
 #include "MyGame.h"
-
-std::vector<std::string> tiles = { "img/wall.bmp", "img/floor.bmp" };
 
 std::vector<std::vector <int>> tileMap = {
 {1,1,1,0,1,0,1,1,1,1},
@@ -18,6 +17,7 @@ std::vector<std::vector <int>> tileMap = {
 };
 
 MyGame::MyGame() {
+  hero_loc = { 0, 0 };
 }
 
 void MyGame::init(GameContext& context) {
@@ -26,8 +26,12 @@ void MyGame::init(GameContext& context) {
   context.load_file("img/hero-down.bmp");
 }
 
-std::vector<int> MyGame::hero_location() {
-  return{ 0, 0 };
+void MyGame::set_hero_loc(int x, int y) {
+  hero_loc = { x, y };
+}
+
+std::vector<int> MyGame::get_hero_location() {
+  return hero_loc;
 }
 
 
@@ -40,8 +44,25 @@ void MyGame::render(GameContext& context) {
         context.draw_sprite("img/floor.bmp", j * 72, i * 72);
       }
   }
-  context.draw_sprite("img/hero-down.bmp", hero_location()[1] * 72, hero_location()[0] * 72);
+  context.draw_sprite("img/hero-down.bmp", get_hero_location()[1] * 72, get_hero_location()[0] * 72);
+  if (context.was_key_pressed(ARROW_DOWN)) {
+    std::cout << "arrow-down\n";
+    set_hero_loc(get_hero_location()[0] + 1, get_hero_location()[1]);
+   // context.render();
+    context.draw_sprite("hero-down.bmp", get_hero_location()[1] * 72, get_hero_location()[0] * 72);
+   // context.reset_keys();
+    context.on_key_down(ARROW_DOWN);
+  }
   context.render(); 
+}
+
+void MyGame::move(GameContext& context) {
+  if (context.was_key_pressed(ARROW_DOWN)) {
+    std::cout << "arrow-down\n";
+    set_hero_loc(get_hero_location()[0], get_hero_location()[1 + 1]);
+  }
+  context.draw_sprite("img/hero-down.bmp", get_hero_location()[1] * 72, get_hero_location()[0] * 72);
+  context.render();
 }
 
 MyGame::~MyGame() {
--- week-07/RPG/MyGame.h
+++ week-07/RPG/MyGame.h
@@ -1,18 +1,20 @@
 // #pragma once
 #ifndef MYGAME_H
 #define MYGAME_H
+#include <vector>
 #include "game-engine2.h"
 
 class MyGame : public Game {
 protected:
-  int tileMapWidth = 8;
-  int tileMapHeight = 8;
+  std::vector<int> hero_loc;
 public:
   MyGame();
   ~MyGame();
   virtual void init(GameContext& context);
   virtual void render(GameContext& context);
-  std::vector<int> hero_location();
+  void set_hero_loc(int x, int y);
+  std::vector<int> get_hero_location();
+  void move(GameContext& context);
 };
 
 #endif
