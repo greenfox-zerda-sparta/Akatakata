b0ec5d29bbdb21f77e0e2dbb41b52e671118726e
Kata Almasy <almasy.kata@gmail.com>
2016-12-16 11:59:28 +0100

    SDL part cleared a bit more

--- week-08/day-4/Recursion/main.cpp
+++ week-08/day-4/Recursion/main.cpp
@@ -15,21 +15,21 @@ int main(int argc, char ** argv) {
   SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
   SDL_SetRenderDrawColor(renderer, 19, 19, 70, 0);
   SDL_RenderClear(renderer);
+  
   while (!quit) {
     SDL_WaitEvent(&event);
     if (event.type == SDL_QUIT) {
       quit = true;
       break;
     }
-
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
     draw d(0, 0, 0);
     d.grid(renderer, 0, 0, line);
     SDL_RenderPresent(renderer);
   }
+
   SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(window);
   SDL_Quit();
-  
   return 0;
 }
