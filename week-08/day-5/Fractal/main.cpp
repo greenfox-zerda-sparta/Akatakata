173585192744987dcf29b696417ea56c27afdbbe
Kata Almasy <almasy.kata@gmail.com>
2016-12-16 16:05:30 +0100

    reorganizing

--- week-08/day-5/Fractal/main.cpp
+++ week-08/day-5/Fractal/main.cpp
@@ -9,14 +9,14 @@ int main(int argc, char ** argv) {
 
   SDL_Init(SDL_INIT_EVERYTHING); 
 
-  SDL_Window* window = SDL_CreateWindow("SDL2 Fractal",
+  SDL_Window* window = SDL_CreateWindow("SDL2 Fractals - press A for triangles, press S for squres",
     SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 800, 0);
 
   SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
   SDL_SetRenderDrawColor(renderer, 19, 19, 70, 0);
   SDL_RenderClear(renderer);
   draw d(0, 0, 0);
-  
+
   while (!quit) {
     SDL_WaitEvent(&event);
     switch (event.type) {
@@ -29,20 +29,19 @@ int main(int argc, char ** argv) {
            quit = true;
            break;
          case SDLK_a:
-         //  SDL_SetRenderDrawColor(renderer, 19, 19, 70, 0);
+           SDL_SetRenderDrawColor(renderer, 19, 19, 70, 0);
            SDL_RenderClear(renderer);
            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
            d.grid(renderer, 0, 0, line);
            break;
          case SDLK_s:
-         //  SDL_SetRenderDrawColor(renderer, 19, 19, 70, 0);
+           SDL_SetRenderDrawColor(renderer, 19, 19, 70, 0);
            SDL_RenderClear(renderer);
            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-           d.sierpinsky_triangle(renderer, 30, 50, 739);
+           d.sierpinsky_triangle(renderer, 10, 50, line - 20);
            break;
        }
     }
-
     SDL_RenderPresent(renderer);
   }
   SDL_DestroyRenderer(renderer);
