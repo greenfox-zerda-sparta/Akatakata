6e4dca4dd00006eb8f4c366ca04f1436618b33b9
Kata Almasy <almasy.kata@gmail.com>
2016-12-19 12:09:38 +0100

    2 pictures in main window

--- week-08/day-5/Fractal/main.cpp
+++ week-08/day-5/Fractal/main.cpp
@@ -2,6 +2,11 @@
 #include <SDL.h>
 #include "draw.h"
 
+SDL_Rect squares;
+SDL_Rect triangles;
+
+
+
 int main(int argc, char ** argv) {
   int line = 799;
   bool quit = false;
@@ -15,6 +20,32 @@ int main(int argc, char ** argv) {
   SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
   SDL_SetRenderDrawColor(renderer, 19, 19, 70, 0);
   SDL_RenderClear(renderer);
+
+  SDL_Surface* surface = SDL_LoadBMP("squares.bmp");
+  if (surface == nullptr) {
+    std::cout << "Failed to load surface " << std::endl;
+  }
+  squares.x = 200;
+  squares.y = 200;
+  squares.w = 150;
+  squares.h = 150;
+  SDL_Texture* texture_squares = SDL_CreateTextureFromSurface(renderer, surface);
+  SDL_FreeSurface(surface);
+ 
+
+  SDL_Surface* surface2 = SDL_LoadBMP("triangles.bmp");
+  if (surface == nullptr) {
+    std::cout << "Failed to load surface " << std::endl;
+  }
+  triangles.x = 500;
+  triangles.y = 200;
+  triangles.w = 150;
+  triangles.h = 150;
+  SDL_Texture* texture_triangles = SDL_CreateTextureFromSurface(renderer, surface2);
+  SDL_FreeSurface(surface2);
+
+  SDL_RenderCopy(renderer, texture_squares, NULL, &squares);
+  SDL_RenderCopy(renderer, texture_triangles, NULL, &triangles);
   draw d(0, 0, 0);
 
   while (!quit) {
@@ -28,13 +59,13 @@ int main(int argc, char ** argv) {
          case SDLK_ESCAPE:
            quit = true;
            break;
-         case SDLK_a:
+         case SDLK_s:
            SDL_SetRenderDrawColor(renderer, 19, 19, 70, 0);
            SDL_RenderClear(renderer);
            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
            d.grid(renderer, 0, 0, line);
            break;
-         case SDLK_s:
+         case SDLK_a:
            SDL_SetRenderDrawColor(renderer, 19, 19, 70, 0);
            SDL_RenderClear(renderer);
            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
@@ -44,6 +75,8 @@ int main(int argc, char ** argv) {
     }
     SDL_RenderPresent(renderer);
   }
+  SDL_DestroyTexture(texture_squares);
+  SDL_DestroyTexture(texture_triangles);
   SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(window);
   SDL_Quit();
