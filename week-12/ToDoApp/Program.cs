828d4e0ae44d8db1e5741c9acee3fa3106afd699
Kata Almasy <almasy.kata@gmail.com>
2017-01-18 11:51:38 +0100

    todo app list handling, file handling

---
+++ week-12/ToDoApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoApp {
+  class Program {
+    static void Main(string[] args) {
+      TaskHandler tasks = new TaskHandler();
+      tasks.PrintList();
+      Console.WriteLine();
+      Console.WriteLine("Type the task to add: ");
+      string toAdd = Console.ReadLine();
+      tasks.AddTask(toAdd);
+      Console.ReadLine();
+    }
+  }
+}
---
+++ week-12/ToDoApp/TaskHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ToDoApp {
+  class TaskHandler {
+    private string filepath = @"C:\\greenfox/Akatakata/week-12/ToDoApp/todolist.txt";
+    public string Filepath { get; set; }
+
+    private List<string> todolist = new List<string>();
+
+    public void ReadFromFile() {
+      int counter = 0;
+      string line;
+      StreamReader file = new StreamReader(filepath);
+      while ((line = file.ReadLine()) != null) {
+        string task = "[ ] " + (counter + 1).ToString() + " - " + line;
+        todolist.Add(task);
+        counter++;
+      }
+      file.Close();
+    }
+
+    public void PrintList() {
+      ReadFromFile();
+      foreach (string task in todolist) {
+        Console.WriteLine(task);
+      }
+    }
+
+    public void AddTask(string taskToAdd) {
+      using (StreamWriter file = File.AppendText(filepath)) {
+        Console.WriteLine();
+        file.WriteLine(taskToAdd);
+      }
+    }
+  }
+}
