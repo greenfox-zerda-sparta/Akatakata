17d2d0463f2d1d0c89ddd5407d658889532633db
Akatakata <almasy.kata@gmail.com>
2017-01-19 15:37:56 +0100

    Basic C# chat client, no while loop, sends and receives 1 message after connection.
    Basic C# server, handles 1 client.

---
+++ week-12/Chat/Client/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class SynchronousSocketClient {
+
+  public static void StartClient() {
+    // Data buffer for incoming data.
+    byte[] bytes = new byte[1024];
+
+    try {
+      // Establish the remote endpoint for the socket.
+      IPHostEntry ipHostInfo = Dns.Resolve(Dns.GetHostName());
+      IPAddress ipAddress = ipHostInfo.AddressList[0];
+      IPEndPoint remoteEndpoint = new IPEndPoint(ipAddress, 11000);
+
+      // Create a TCP/IP  socket.
+      Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+      // Connect the socket to the remote endpoint. Catch any errors.
+      try {
+        client.Connect(remoteEndpoint);
+        Console.WriteLine("Socket connected to {0}", client.RemoteEndPoint.ToString());
+
+        // Send: Encode the data string into a byte array and Send
+        byte[] msg = Encoding.ASCII.GetBytes("SUBIDUBIDUUUU");
+        int bytesSent = client.Send(msg);
+
+        // Receive
+        int bytesRec = client.Receive(bytes);
+        Console.WriteLine("Echoed test = {0}", Encoding.ASCII.GetString(bytes, 0, bytesRec));
+
+        // Release and close the socket.
+        client.Shutdown(SocketShutdown.Both);
+        client.Close();
+
+      } catch (ArgumentNullException ane) {
+        Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+      } catch (SocketException se) {
+        Console.WriteLine("SocketException : {0}", se.ToString());
+      } catch (Exception e) {
+        Console.WriteLine("Unexpected exception : {0}", e.ToString());
+      }
+
+    } catch (Exception e) {
+      Console.WriteLine(e.ToString());
+    }
+  }
+
+  public static int Main(String[] args) {
+    StartClient();
+    return 0;
+  }
+}
---
+++ week-12/Chat/Server/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class SynchronousSocketServer {
+
+  // Incoming data from the client.
+  public static string data = null;
+
+  public static void StartListening() {
+    // Data buffer for incoming data.
+    byte[] bytes = new Byte[1024];
+
+    // Establish the local endpoint for the socket.
+    IPHostEntry ipHostInfo = Dns.Resolve("10.27.6.82");
+    IPAddress ipAddress = ipHostInfo.AddressList[0];
+    IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+
+    // Create a TCP/IP socket.
+    Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+    // Bind the socket to the local endpoint and listen for incoming connections.
+    try {
+      listener.Bind(localEndPoint);
+      listener.Listen(10);
+
+      // Start listening for connections and remains running
+      while (true) {
+
+        Console.WriteLine("Waiting for a connection...");
+        // Program is suspended while waiting for an incoming connection.
+        Socket handler = listener.Accept();
+        data = null;
+
+        // An incoming connection needs to be processed.
+        while (true) {
+          bytes = new byte[1024];
+          int bytesRec = handler.Receive(bytes);
+          data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+          if (data.Length > 0) {
+            break;
+          }
+        }
+
+        // Show the data on the console.
+        Console.WriteLine("Text received : {0}", data);
+
+        // Echo the data back to the client.
+        byte[] msg = Encoding.ASCII.GetBytes(data);
+
+        handler.Send(msg);
+        handler.Shutdown(SocketShutdown.Both);
+        handler.Close();
+      }
+
+    } catch (Exception e) {
+      Console.WriteLine(e.ToString());
+    }
+
+    Console.WriteLine("\nPress ENTER to continue...");
+    Console.Read();
+
+  }
+
+  public static int Main(String[] args) {
+    StartListening();
+    return 0;
+  }
+}
