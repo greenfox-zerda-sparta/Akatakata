759d38fdb51e82d6dfe62c48a4bd2ae820d7e781
Akatakata <almasy.kata@gmail.com>
2017-01-20 11:04:31 +0100

    C# Chat: conitnuous chatting solved, client ready for sending username upon request from server

--- week-12/Chat/Client/Program.cs
+++ week-12/Chat/Client/Program.cs
@@ -3,64 +3,14 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
-public class SynchronousSocketClient {
+namespace Chatclient {
+  class Program {
 
-  public static void StartClient() {
-    bool shutdown = false;
-    // Data buffer for incoming data.
-    byte[] bytes = new byte[1024];
-
-    try {
-      // Establish the remote endpoint for the socket.
-      IPHostEntry ipHostInfo = Dns.Resolve(Dns.GetHostName());
-      IPAddress ipAddress = ipHostInfo.AddressList[0];
-      IPEndPoint remoteEndpoint = new IPEndPoint(ipAddress, 11000);
-
-      // Create a TCP/IP  socket.
-      Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-
-      // Connect the socket to the remote endpoint. Catch any errors.
-      try {
-        client.Connect(remoteEndpoint);
-        Console.WriteLine("Socket connected to {0}", client.RemoteEndPoint.ToString());
-
-        do {
-
-          // Receive
-           if (client.Poll(100, SelectMode.SelectRead)) {
-            bytes = new byte[1024];
-            int bytesRec = client.Receive(bytes);
-            if (bytesRec > 0) {
-              Console.WriteLine("Echoed test = {0}", Encoding.ASCII.GetString(bytes, 0, bytesRec));
-            }
-          }
-
-          // Send: Encode the data string into a byte array and Send
-            string message = Console.ReadLine();
-            byte[] msg = Encoding.ASCII.GetBytes(message);
-            int bytesSent = client.Send(msg);
-
-
-        } while (shutdown == false);
-        // Release and close the socket.
-        client.Shutdown(SocketShutdown.Both);
-        client.Close();
-
-      } catch (ArgumentNullException ane) {
-        Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
-      } catch (SocketException se) {
-        Console.WriteLine("SocketException : {0}", se.ToString());
-      } catch (Exception e) {
-        Console.WriteLine("Unexpected exception : {0}", e.ToString());
-      }
-
-    } catch (Exception e) {
-      Console.WriteLine(e.ToString());
+    public static int Main(String[] args) {
+      Client myClient = new Client();
+      myClient.Run();
+      return 0;
     }
-  }
 
-  public static int Main(String[] args) {
-    StartClient();
-    return 0;
   }
 }
--- week-12/Chat/Server/Program.cs
+++ week-12/Chat/Server/Program.cs
@@ -2,70 +2,15 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using System.Collections.Generic;
 
-public class SynchronousSocketServer {
 
-  // Incoming data from the client.
-  public static string data = null;
+namespace Chatserver {
+  class Program {
 
-  public static void StartListening() {
-
-    byte[] bytes = new Byte[1024];
-
-    IPHostEntry ipHostInfo = Dns.Resolve(Dns.GetHostName());
-    IPAddress ipAddress = ipHostInfo.AddressList[0];
-    IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
-
-    Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-    List<Socket> SocketList = new List<Socket>();
-
-    try {
-      listener.Bind(localEndPoint);
-      listener.Listen(10);
-
-      Console.WriteLine("Waiting for a connection...");
-      while (true) {
-      //  if (listener.Available > 0) {
-          SocketList.Add(listener.Accept());
-          Console.WriteLine("Connected!");
-        Console.WriteLine(SocketList.Count);
-        //     listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-        //   }
-
-        //while (true) {
-        //  data = null;
-        //  for (int i = 0; i < SocketList.Count; i++) {
-        //    if (SocketList[i].Available > 0) {
-        //      bytes = new byte[1024];
-        //      int bytesRec = SocketList[i].Receive(bytes);
-        //      data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
-        //      if (data.Length > 0) {
-        //        break;
-        //      }
-        //    }
-        //  }
-        //}
-
-        //Console.WriteLine("Text received : {0}", data);
-        //byte[] msg = Encoding.ASCII.GetBytes(data);
-
-        //SocketList[0].Send(msg); // HA TOBB SOCKET VAN, VEGIG KELL NEZNI
-      }
-     // handler.Shutdown(SocketShutdown.Both);
-     // handler.Close();
-
-    } catch (Exception e) {
-      Console.WriteLine(e.ToString());
+    public static int Main(String[] args) {
+      Server myServer = new Server();
+      myServer.Run();
+      return 0;
     }
-
-    Console.WriteLine("\nPress ENTER to continue...");
-    Console.Read();
-
-  }
-
-  public static int Main(String[] args) {
-    StartListening();
-    return 0;
   }
 }
